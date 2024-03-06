b2697419d6c40f030b0fb30d82f633b1b5899d82
Cloudpb Du <144001918+richarddu1016@users.noreply.github.com>
2024-03-06 10:51:09 +0800

    Initial commit

---
+++ Library_InterviewTest/src/main/java/org/yunchien/library_interviewtest/LibraryInterviewTestApplication.java
@@ -0,0 +1,13 @@
+package org.yunchien.library_interviewtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryInterviewTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryInterviewTestApplication.class, args);
+    }
+
+}
---
+++ Library_InterviewTest/src/test/java/org/yunchien/library_interviewtest/LibraryInterviewTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yunchien.library_interviewtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryInterviewTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
